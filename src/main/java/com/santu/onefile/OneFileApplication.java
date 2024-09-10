d9f1efba64e6105393fbceed02a952f108fe2460
zhangmanyue774 <960001948@qq.com>
2024-09-10 19:10:33 +0800

    新建项目

---
+++ src/main/java/com/santu/onefile/OneFileApplication.java
@@ -0,0 +1,11 @@
+package com.santu.onefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneFileApplication.class, args);
+    }
+}
---
+++ src/test/java/com/santu/onefile/OneFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santu.onefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
